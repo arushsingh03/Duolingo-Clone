ea0958f41cace5ae180eabb04a6c45e33141394d
Arush Singh <sarushmad@gmail.com>
2024-05-10 13:56:34 +0530

    updated footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -14,7 +14,7 @@ export default function Home (){
                 <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
                     Learn, Practice, and Master new languages with Lingo.
                 </h1>
-                <div>
+                <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
                     <ClerkLoading>
                         <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
                     </ClerkLoading>
@@ -26,7 +26,7 @@ export default function Home (){
                                 </Button>
                           </SignUpButton>
                           <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
-                                <Button variant={"classic"} size="lg" className="w-full" >
+                                <Button variant={"primaryOutline"} size="lg" className="w-full" >
                                     I already have an account
                                 </Button>
                           </SignInButton>
