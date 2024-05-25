d7d84222bab62246c3d6c035ff7e6d58c19a0e29
Arush Singh <sarushmad@gmail.com>
2024-05-25 17:36:10 +0530

    created user-progress page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,8 +3,21 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
 
 import { Header } from "./header";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-const LearnPage= () => {
+const LearnPage= async () => {
+  const userProgressData = getUserProgress();
+
+  const [
+    userProgress
+  ] =await Promise.all([
+    userProgressData
+  ]);
+
+  if (!userProgress || !userProgress.activeCourse){
+    redirect("/courses");
+  }
 return ( 
     <div className="flex flex-row-reverse gap-[48px] px-6">
         <StickyWrapper>
