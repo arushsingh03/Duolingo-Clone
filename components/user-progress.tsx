e4dd2b13e4e1cb3562b7b627f44a9dce5de55950
Arush Singh <sarushmad@gmail.com>
2024-05-26 13:29:30 +0530

    created n successfully pushed all the required field of courses page.

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,11 +1,13 @@
 
-import { Button } from "@/components/ui/button";
-import { InfinityIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+
+import { courses } from "@/db/schema";
+import { InfinityIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type Props= {
-    activeCourse:{imageSrc: string; title:string}; /// TODO Replace with DB types
+    activeCourse: typeof courses.$inferSelect;
     hearts: number;
     points:number;
     hasActiveSubscription:boolean;
